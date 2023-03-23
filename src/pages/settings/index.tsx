958d3ddadc9b0a18c278d44939dfa455375d6e31
Valentin Vogt <vogtva@student.ethz.ch>
2023-03-23 20:23:29 +0100

    Values to SVG Roughly working

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,12 +1,53 @@
+import {getEODEquidistant} from "@/utils/six";
+import {query_six} from "@/utils/six";
+import {toString} from "autoprefixer";
+import {useEffect, useState} from "react";
+
+function phi(i, fi) {
+    let x = Number(i)
+    let fx = Number(fi)
+    return (4 * x).toString() + " " + Math.round(fx).toString()
+}
+
+export function getStockPrice(ticker: string, from: string, to: string) {
+
+    let uri = `https://web.api.six-group.com/api/findata/v1/listings/marketData/eodTimeseries?scheme=TICKER_BC&ids=${ticker}&from=${from}`
+    return query_six(uri).then(data => {
+        console.log(data.data.listings[0].marketData.eodTimeseries)
+        let datae = data.data.listings[0].marketData.eodTimeseries
+        let res = "M " + phi(0, datae[0].close) + " "
+        for (let i in datae) {
+            if (datae[i].close) {
+                res += "L " + phi(i, datae[i].close) + " "
+            }
+        }
+        // console.log(data["listings"])
+        return res
+    })
+
+}
+
 function Settings() {
 
-    return (
-        <div>
+    const [path, setPath] = useState("")
 
-        <h1>Nothing to see here! </h1>
-        </div>
 
-    )
+    getStockPrice("AAPL_67", "2023-01-30", "2023-01-30").then(res => {
+        setPath(res)
+    })
+    console.log(path)
+    return (<svg className={"w-full"} height="500" xmlns="http://www.w3.org/2000/svg">
+
+        <path d={path} fill="transparent" stroke="black"></path>
+    </svg>)
+    // console.log(getEODEquidistant("AAPL_67", "2023-01-30", "2023-01-30", 30))
+    // return (
+    //     <div>
+    //
+    //     <h1>Nothing to see here! </h1>
+    //     </div>
+    //
+    // )
 }
 
 export default Settings
---
+++ src/utils/data_to_svg/six.js
@@ -0,0 +1,62 @@
+export function query_six(uri) {
+    return fetch(uri, {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "api-version": "2023-01-01",
+            "Accept-Encoding": "gzip",
+            "Accept": "application/json"
+        }
+    }).then((response) => response.json())
+}
+
+export function interpolate_dates(start_date, end_date, num_tick_marks) {
+    let dates_interpolated = []
+
+    const start_date_obj = new Date(start_date)
+    const end_date_obj = new Date(end_date)
+    const time_diff = end_date_obj.getTime() - start_date_obj.getTime()
+    const time_diff_days = time_diff / (1000 * 3600 * 24)
+    const time_diff_days_per_tick = time_diff_days / num_tick_marks
+
+    let prev_month = -1
+    for (let i = 0; i < num_tick_marks; i++) {
+        const date_obj = new Date(start_date_obj.getTime() + (time_diff_days_per_tick * i * 1000 * 3600 * 24))
+        dates_interpolated.push(date_obj.toISOString().split('T')[0])
+    }
+
+    return dates_interpolated
+
+}
+
+export function getEODEquidistant(ticker, start, end, points) {
+
+    let dates = interpolate_dates(start, end, points)
+    console.log(dates)
+    let uri = `https://web.api.six-group.com/api/findata/v1/listings/marketData/eodTimeseries?scheme=TICKER_BC&ids=${ticker}&from=${start}&to=${end}`
+    return query_six(uri).then(data => {
+
+        let formatted = []
+        let first = null;
+        data.data?.listings[0].marketData.eodTimeseries.forEach(v => {
+            if (dates.includes(v.sessionDate) && v.close) {
+
+                formatted.push({
+                    date: v.sessionDate,
+                    stock: ticker,
+                    price: v.close,
+                    normalized: first === null ? 1 : v.close / first
+                })
+
+                if (first == null)
+                    first = v.close;
+            }
+        })
+
+        return formatted;
+
+    })
+
+}
