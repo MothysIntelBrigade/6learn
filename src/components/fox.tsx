4c9281842dbc6d2548335188f031005b9ef10f51
Valentin Vogt <me@student.ethz.ch>
2023-03-23 11:53:40 +0100

    Fox jumps up, has text box

--- src/components/fox.tsx
+++ src/components/fox.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useState } from 'react';
+import { Popover } from '@headlessui/react'
 
 export default function Footer() {
     return (
@@ -9,19 +10,39 @@ export default function Footer() {
     );
 }
 
+function FoxText() {
+    return (
+        <Popover className="absolute -top-14 bg-blue-100 rounded-md border border-black">
+            <Popover.Button>I am the fox</Popover.Button>
+
+            <Popover.Panel className="absolute z-10">
+                <div className="grid grid-cols-2">
+                    <a href="/analytics">Analytics</a>
+                    <a href="/engagement">Engagement</a>
+                    <a href="/security">Security</a>
+                    <a href="/integrations">Integrations</a>
+                </div>
+            </Popover.Panel>
+        </Popover>
+    )
+}
+
 function Fox() {
     const [up, setUp] = useState(false);
 
     function handleClick() {
         setUp(!up);
     }
-    let style = (up ?  'fixed bottom-0 right-20'  :  'fixed -bottom-12 right-20' );
+    // Jump up and down
+    // TODO: Animate more nicely
+    let style = (up ?  ' bottom-0 ':' -bottom-14 ' );
     return (
-        <>
-            <div className={style}>
-                    <Image src="/fox.jpg" alt="logo" width="128" height="64"  onClick={handleClick}/>
+        <div className='p-0' >
+            <div className={'fixed' + style + 'right-[10%]'}>
+                {up && <FoxText />}
+                <Image src="/fox.png" alt="fox" width="120" height="64"  onClick={handleClick}/>
             </div>
-        </>
+        </div>
 
     )
 }
