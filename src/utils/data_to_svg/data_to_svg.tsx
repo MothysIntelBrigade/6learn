ba48144dda441939b59bcbebe7f147f8e13191d3
Valentin Vogt <vogtva@student.ethz.ch>
2023-03-23 21:07:58 +0100

    Values to SVG working with width, height. Demo in settings

---
+++ src/utils/data_to_svg/data_to_svg.tsx
@@ -0,0 +1,33 @@
+import {query_six} from "@/utils/six";
+
+function phi(i: string, fi: string, dx: number, dy: number, h: number, ymin: number) {
+    let x = Number(i)
+    let fx = Number(fi)
+    return Math.round(dx * x).toString() + " " + Math.round(h - dy * (fx - ymin)).toString()
+}
+
+export async function svgPathMaker(ticker: string, from: string, to: string, width: number, height: number) {
+    let uri = `https://web.api.six-group.com/api/findata/v1/listings/marketData/eodTimeseries?scheme=TICKER_BC&ids=${ticker}&from=${from}`
+    return query_six(uri).then(data => {
+        let ts = data.data.listings[0].marketData.eodTimeseries
+        let dx = width / ts.length
+        let max = 0
+        let min = 1000
+        for (let i in ts) {
+            if (Number(ts[i].close) > max) max = Number(ts[i].close);
+            if (Number(ts[i].close) < min) min = Number(ts[i].close);
+        }
+        console.log("Max", max, min)
+        let dy = height / (max - min)
+        console.log(dx, dy)
+        let res = "M " + phi("0", ts[0].close, dx, dy, height, min) + " "
+        for (let i in ts) {
+            if (ts[i].close) {
+                res += "L " + phi(i, ts[i].close, dx, dy, height, min) + " "
+            }
+        }
+        console.log(data["listings"])
+        return res
+    })
+
+}
