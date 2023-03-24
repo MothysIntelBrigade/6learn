d64d676cfc84b44aaadcfdda9227c49d00686f43
Benedict Armstrong <contact@benarmstro.ng>
2023-03-24 07:08:24 +0100

    chnages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,17 +18,17 @@ export default function Home() {
                     <button
                         className="grid place-items-center fixed mt-5 p-1 top-[20%] bottom-[40%] left-[55%] right-[11%] bg-white rounded-md border border-gray-500 shadow-lg">
                         <GraphUpArrow/>
-                        <p className={" text-3xl italic text-black m-0 h-[52px] font-['Inter'] leading-[normal]"}> {"Learn"} </p>
+                        <p className={" text-3xl italic text-black m-0 h-[52px]  leading-[normal]"}> {"Learn"} </p>
                     </button>
                 </Link>
                 <button
                     className="grid place-items-center fixed mt-5 p-1 top-[20%] bottom-[40%] left-[11%] right-[55%] bg-white rounded-md border border-gray-500 shadow-lg">
                     <Trophy/>
-                    <p className={" text-3xl italic text-black m-0 h-[52px] font-['Inter'] leading-[normal]"}> {"Daily"} </p>
+                    <p className={" text-3xl italic text-black m-0 h-[52px] leading-[normal]"}> {"Daily"} </p>
                 </button>
 
                 <div className={"absolute top-2/3 grid place-items-center"}>
-                    <p className={"text-3xl italic top-[66%] font-['Noto_Sans'] text-[rgba(208,57,38,1)]"}>{"powered by"} </p>
+                    <p className={"text-3xl italic top-[66%] text-[rgba(208,57,38,1)]"}>{"powered by"} </p>
                     <br/><br/>
                     <Image src="/logo.png" alt="logo" width="86" height="86"/>
                 </div>
